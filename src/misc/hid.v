c0dc2dc3d56d73bb4682825519f66ce3fc29acbd
Stefan Voss <75468830+vossstef@users.noreply.github.com>
2024-01-22 21:02:01 +0100

    mouse fix (#29)
    c1351 Mouse emulation using a USB Mouse
    video centered

--- src/misc/hid.v
+++ src/misc/hid.v
@@ -2,6 +2,8 @@
     hid.v
  
     hid (keyboard, mouse etc) interface to the IO MCU
+
+    c64 core specific variant of hid
   */
 
 module hid (
@@ -23,18 +25,14 @@ module hid (
   output reg [7:0] numpad,
   input  [7:0] keyboard_matrix_out,
   output [7:0] keyboard_matrix_in,
+  output reg   key_restore,
   reg [1:0]    mouse_btns,
-  reg [7:0]    mouse_x_cnt,
-  reg [7:0]    mouse_y_cnt,
+  reg [7:0]    mouse_x,
+  reg [7:0]    mouse_y,
   reg          mouse_strobe
 );
 
 reg [7:0] keyboard[7:0]; // array of 8 elements of width 8bit
-
-reg [1:0] mouse_x;
-reg [1:0] mouse_y;
-
-//assign mouse = { mouse_btns, mouse_x, mouse_y };
 
 //keyboard 
 assign keyboard_matrix_in =
@@ -47,9 +45,6 @@ assign keyboard_matrix_in =
 	      (!keyboard_matrix_out[6]?keyboard[6]:8'hff)&
 	      (!keyboard_matrix_out[7]?keyboard[7]:8'hff);
 
-// limit the rate at which mouse movement data is sent to the
-// ikbd
-reg [13:0] mouse_div;
 reg [3:0] state;
 reg [7:0] command;  
 reg [7:0] device;   // used for joystick
@@ -60,10 +55,10 @@ reg [5:0] db9_portD;
 always @(posedge clk) begin
    if(reset) begin
       state <= 4'd0;
-      mouse_div <= 14'd0;
       mouse_strobe <=1'b0;
       irq <= 1'b0;
       irq_enable <= 1'b0;
+      key_restore <= 1'b0;
 
       // reset entire keyboard to 1's
       keyboard[ 0] <= 8'hff; keyboard[ 1] <= 8'hff; keyboard[ 2] <= 8'hff;
@@ -106,11 +101,12 @@ always @(posedge clk) begin
             // CMD 2: mouse data
             if(command == 8'd2) begin
                 if(state == 4'd1) mouse_btns <= data_in[1:0];
-                if(state == 4'd2) mouse_x_cnt <= mouse_x_cnt + data_in;
+                if(state == 4'd2) mouse_x <= data_in;
                 if(state == 4'd3) begin 
-                    mouse_y_cnt <= mouse_y_cnt + data_in; 
-                    mouse_strobe <=1'b1; end
-                                end
+                    mouse_y <= data_in; 
+                    mouse_strobe <=1'b1; 
+                end
+            end
 
             // CMD 3: receive digital joystick data
             if(command == 8'd3) begin
@@ -118,43 +114,17 @@ always @(posedge clk) begin
                 if(state == 4'd2) begin
                     if(device == 8'd0) joystick0 <= data_in;
                     if(device == 8'd1) joystick1 <= data_in;
-                    if(device == 8'h80) numpad <= data_in;  // 0, 0, KP * button2, KP0 trigger, KP 8 up, KP 2 down, KP 4 left, KP 6 right
-                end                                            
+                    if(device == 8'h80) begin 
+                             numpad <= data_in; 
+                             key_restore <= data_in[6]; 
+                          end // 0, 0, KP * button2, KP0 trigger, KP 8 up, KP 2 down, KP 4 left, KP 6 right
+                end
             end
 
             // CMD 4: send digital joystick data to MCU
             if(command == 8'd4) begin
                 if(state == 4'd1) irq_enable <= 1'b1;    // (re-)enable interrupt
                 data_out <= {2'b00, db9_port };               
-            end
-
-        end
-      end else begin // if (data_in_strobe)
-        mouse_div <= mouse_div + 14'd1;      
-        if(mouse_div == 14'd0) begin
-            if(mouse_x_cnt != 8'd0) begin
-                if(mouse_x_cnt[7]) begin
-                    mouse_x_cnt <= mouse_x_cnt + 8'd1;
-                    // 2 bit gray counter to emulate the mouse's light barriers
-                    mouse_x[0] <=  mouse_x[1];
-                    mouse_x[1] <= ~mouse_x[0];		  
-                end else begin
-                    mouse_x_cnt <= mouse_x_cnt - 8'd1;
-                    mouse_x[0] <= ~mouse_x[1];
-                    mouse_x[1] <=  mouse_x[0];
-                end	    
-            end // if (mouse_x_cnt != 8'd0)
-	    
-            if(mouse_y_cnt != 8'd0) begin
-                if(mouse_y_cnt[7]) begin
-                    mouse_y_cnt <= mouse_y_cnt + 8'd1;
-                    mouse_y[0] <=  mouse_y[1];
-                    mouse_y[1] <= ~mouse_y[0];		  
-                end else begin
-                    mouse_y_cnt <= mouse_y_cnt - 8'd1;
-                    mouse_y[0] <= ~mouse_y[1];
-                    mouse_y[1] <=  mouse_y[0];
-                end	    
             end
         end
       end
